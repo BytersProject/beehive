688b8da9b1f9c0a607a650f5a3578c4a5cc781ec
QuantumlyTangled <ethoarx@gmail.com>
2020-11-21 10:18:57 +0100

    refactor: remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,3 @@
-import { Bento, FSComponentLoader } from '@ayanaware/bento';
-import Beehive, { BeehiveVariable } from './lib';
-
 export * from './lib';
 
 export const main = () => 'this builds and pushes';
-
-const bento = new Bento();
-
-(async () => {
-	const beehive = new Beehive({ type: 'user' });
-
-	bento.setVariable(BeehiveVariable.BEEHIVE_TOKEN, 'no u');
-	bento.setVariable(BeehiveVariable.BEEHIVE_DEFAULT_PREFIX, '!');
-
-	const fsloader = new FSComponentLoader();
-	await fsloader.addDirectory(__dirname, 'commands');
-
-	await bento.addPlugins([fsloader, beehive]);
-
-	await bento.verify();
-})().catch(e => {
-	console.log(e);
-	process.exit(1);
-});
